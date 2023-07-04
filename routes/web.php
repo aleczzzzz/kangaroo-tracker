d9ce16289f8e7890badbfdfec7deaa1192561f10
aleczbautista <alecbau60@gmail.com>
2023-07-05 06:21:35 +0800

    home redirect to kangaroos page

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('kangaroos.indexPage');
 });
 
 Route::get('kangaroos', [KangarooController::class, 'indexPage'])->name('kangaroos.indexPage');
